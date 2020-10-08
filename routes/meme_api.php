3244f2759820597e6e886e77f124987c50045222
lantrinh1999 <linhtd.contact@gmail.com>
2020-10-09 03:58:00 +0700

    update api

--- routes/meme_api.php
+++ routes/meme_api.php
@@ -33,6 +33,17 @@ Route::get('/memes/{page?}', function ($page = 1) {
     $collection = collect($memes->toArray());
     $memes = $collection->map(function ($item, $key) {
         $item['meme_meta'] = array_combine(array_column($item['meme_meta'], 'key'), array_column($item['meme_meta'], 'value'));
+
+        $item['tags'] = array_map(function ($tag) {
+            return $tag = [
+                'slug' => $tag['slug'],
+                'name' => $tag['name'],
+            ];
+        }, $item['tags']);
+        $item['created_at'] = date("Y-m-d H:i:s", strtotime($item['created_at']));
+        $item['updated_at'] = date("Y-m-d H:i:s", strtotime($item['updated_at']));
+        unset($item['image']);
+        unset($item['deleted_at']);
         // $item['tags'] = array_combine(array_column($item['tags'], 'slug'), array_column($item['tags'], 'name'));
         return $item;
     });
