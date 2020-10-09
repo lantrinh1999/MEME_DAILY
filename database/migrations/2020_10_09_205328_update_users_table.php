d4c480bb2cc641e96ab63295de8cb19529ef0b69
lantrinh1999 <linhtd.contact@gmail.com>
2020-10-10 04:26:30 +0700

    code

--- database/migrations/2020_10_09_205328_update_users_table.php
+++ database/migrations/2020_10_09_205328_update_users_table.php
@@ -14,7 +14,7 @@ class UpdateUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            if (!Schema::hasColumns('users', 'name')){
+            if (!Schema::hasColumns('users', ['name'])){
                 $table->string('name');
             }
             $table->string('name')->nullable()->change();
