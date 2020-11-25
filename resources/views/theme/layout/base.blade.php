c5c38478b66b0fe45ecfd87a998be3eb29d8e913
lantrinh1999 <linhtd.contact@gmail.com>
2020-11-25 23:53:19 +0700

    code

--- resources/views/theme/layout/base.blade.php
+++ resources/views/theme/layout/base.blade.php
@@ -3,55 +3,57 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="robots" content="index,follow"/>
+    <meta name="google-site-verification" content="0UYbiRDCWN2rv66wNrJskD7XA9X2K_WGx8bWnYUgp7s" />
+    <meta name="robots" content="index,follow" />
     <meta name="dc.language" content="VI">
     <meta name="dc.source" content="//www.memedaily.vn/">
-    <meta name="dc.creator" content="MEMEDAILY"/>
-    <meta name="distribution" content="Global"/>
-    <meta name="revisit" content="1 days"/>
-    <meta name="geo.placename" content="Vietnamese"/>
-    <meta name="geo.region" content="Vietnamese"/>
-    <meta name="generator" content="//www.memedaily.vn"/>
+    <meta name="dc.creator" content="MEMEDAILY" />
+    <meta name="distribution" content="Global" />
+    <meta name="revisit" content="1 days" />
+    <meta name="geo.placename" content="Vietnamese" />
+    <meta name="geo.region" content="Vietnamese" />
+    <meta name="generator" content="//www.memedaily.vn" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
     @yield('title')
-
     @yield('keywords')
-
-    <meta name="description" content="Kho ảnh chế, memes, trào lưu hot và đầy đủ nhất mạng xã hội Việt Nam"/>
-    <meta property="og:description" content="Kho ảnh chế, memes, trào lưu hot và đầy đủ nhất mạng xã hội Việt Nam"/>
-
-    <meta property="og:locale" content="en_US"/>
-    <meta property="og:type" content="object"/>
-
-    <meta property="og:url" content="{{ url()->current() }}"/>
-    <meta property="og:site_name" content="Meme Daily"/>
-
+    <meta name="description" content="Kho ảnh chế, memes, trào lưu hot và đầy đủ nhất mạng xã hội Việt Nam" />
+    <meta property="og:description" content="Kho ảnh chế, memes, trào lưu hot và đầy đủ nhất mạng xã hội Việt Nam" />
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:type" content="object" />
+    <meta property="og:url" content="{{ url()->current() }}" />
+    <meta property="og:site_name" content="Meme Daily" />
     @yield('og:image')
-
     @yield('meta')
-
     <link rel="canonical" href="{{ url()->current() }}">
     <link href="{{ mix('/theme/css/style.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
-          integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <script src="{{ mix('/theme/js/scripts.js') }}" defer></script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-NLWXYQ63G8"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+
+    gtag('config', 'G-NLWXYQ63G8');
+    </script>
+    <script data-ad-client="ca-pub-4818872934671576" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 </head>
 
 <body class="bg-custom h-100">
-@include('theme.layout.header')
-<section class="main py-3 h-100 mb-auto">
-    <div class="container">
-        <div class="row row_has_sidebar">
-            @yield('content')
-            @include('theme.layout.sidebar')
+    @include('theme.layout.header')
+    <section class="main py-3 h-100 mb-auto">
+        <div class="container">
+            <div class="row row_has_sidebar">
+                @yield('content')
+                @include('theme.layout.sidebar')
+            </div>
         </div>
-    </div>
-</section>
-@include('theme.layout.footer')
-{{--<script type="text/javascript" src="./assets/theme/js/scripts.js"></script>--}}
+    </section>
+    @include('theme.layout.footer')
+    {{--<script type="text/javascript" src="./assets/theme/js/scripts.js"></script>--}}
 </body>
 
 </html>
